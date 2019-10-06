5a193b06a3946242b49805d5ba080d755d4a06de
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-06 20:50:05 +0200

    Basic Todo model added

---
+++ models/Todo.ts
@@ -0,0 +1,11 @@
+class Todo {
+  title: string;
+  checked: boolean;
+
+  constructor(title: string, checked: boolean) {
+    this.title = title;
+    this.checked = checked;
+  }
+}
+
+export default Todo;
