6f871b6dcf4b29d0c42b75be1e0bca945b724f4c
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-06 21:02:02 +0200

    Todo List added to the TodosScreen

--- screens/Todos.tsx
+++ screens/Todos.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
 import { View, Text, StyleSheet } from 'react-native';
+
+import List from '../components/Todo/List';
 
 const TodosScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Todos Screen</Text>
+      <View style={styles.content}>
+        <List />
+      </View>
     </View>
   );
 };
@@ -17,6 +21,9 @@ const styles = StyleSheet.create({
     paddingBottom: 40,
     width: '100%',
     height: '100%',
+  },
+  content: {
+    padding: 15,
   },
   heading: {
     fontSize: 40,
