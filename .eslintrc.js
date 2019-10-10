39efdaf4562bd59885a54e239464f6199d41f02d
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-10 17:25:03 +0200

    Basic ESLint config added

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx', '.jsx'] }],
+  },
+};
