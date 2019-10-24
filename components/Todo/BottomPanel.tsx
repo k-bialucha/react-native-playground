4a9325d1bdbbde500ddc4511d33bfb2da77b3196
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-10-25 00:11:44 +0200

    Basic TextInput and Button functionality handled in BottomPanel component

--- components/Todo/BottomPanel.tsx
+++ components/Todo/BottomPanel.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, StyleSheet, TextInput } from 'react-native';
 
 interface Props {
   style: {};
 }
 
-const BottomPanel: React.FC<Props> = ({ style }) => (
-  <View style={style}>
-    <Text>Bottom PANEL</Text>
-  </View>
-);
+const BottomPanel: React.FC<Props> = ({ style }) => {
+  const [text, setText] = useState('');
+
+  return (
+    <View style={{ ...style, ...styles.layout }}>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={value => setText(value)}
+        placeholder="new todo..."
+      />
+      <Button
+        title="+ Add"
+        onPress={() => {
+          console.warn('button press - adding text:', text);
+          setText('');
+        }}
+        disabled={!text}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  layout: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    paddingTop: 10,
+  },
+  textInput: {
+    flex: 1,
+    marginRight: 15,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1,
+  },
+});
 
 export default BottomPanel;
